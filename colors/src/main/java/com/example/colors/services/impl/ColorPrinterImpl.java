477698ad3055a3ac6b583198409d181925ae666d
Thanos Moschou <130168149+thanosmoschou@users.noreply.github.com>
2024-01-02 15:33:10 +0200

    Update ColorPrinterImpl.java

--- colors/src/main/java/com/example/colors/services/impl/ColorPrinterImpl.java
+++ colors/src/main/java/com/example/colors/services/impl/ColorPrinterImpl.java
@@ -45,7 +45,8 @@ public class ColorPrinterImpl implements ColorPrinter
 	 * Let's swap out the dependencies it uses
 	 * 
 	 * The concrete classes are called Beans
-	 * 
+  * 
+  * The objects that form the backbone of your application and that are managed by the Spring     IoC container are called beans
 	 * How do I create Beans? One way is with configuration file.
 	 * Go to the root of the project com.example.colors and create a package and call it for example config.
 	 * Inside the config package create a new class called for example PrinterConfig.
